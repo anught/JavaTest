295429719c14622ab3a9286577723df0775089f9
Administrator <Administrator@AGOE2345-030708>
2020-04-16 10:56:22 +0800

    创建java测试工程

---
+++ src/main/java/TEST/allDemo/App.java
@@ -0,0 +1,15 @@
+package TEST.allDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println();
+        
+    }
+}
