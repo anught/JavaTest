885c44872d0c0f62c998148ff9a695bdd3da0cae
Administrator <Administrator@SC-202003021003>
2020-05-21 06:50:09 +0800

    synchronized

---
+++ src/main/java/syncTest/syncTest3.java
@@ -0,0 +1,50 @@
+package syncTest;
+
+public class syncTest3 {
+
+	public  void m1() {
+		System.out.println("m1 start");
+		
+		try {
+			synchronized(this) {
+				System.out.println("m1 execute");
+				Thread.sleep(2000);	
+			}
+			System.out.println("m1 execute end");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public synchronized void m2() {
+		System.out.println("m2 start");
+		
+		try {
+			synchronized(this) {
+				System.out.println("m2 execute");
+				Thread.sleep(2000);	
+			}
+			System.out.println("m2 execute end");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	public static void main(String[] args) {
+		syncTest3 t1 = new syncTest3();
+		
+		new Thread(()->{
+			t1.m1();			
+		}).start();
+		
+		new Thread(()->{
+			t1.m1();			
+		}).start();
+		
+	}
+	
+
+}
